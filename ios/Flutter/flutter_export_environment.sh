04b173a2f7ed97fcb0b56e84fc126e0358c1677d
冰 <chenrubing@yaodaitong.com.cn>
2024-04-16 14:02:13 +0800

    版本更新

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/rich/fvm/versions/3.0.1"
-export "FLUTTER_APPLICATION_PATH=/Users/rich/Documents/flutterplugin/diox_log"
+export "FLUTTER_ROOT=/Users/bing/fvm/versions/stable"
+export "FLUTTER_APPLICATION_PATH=/Users/bing/Desktop/diox_log"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=2.0.2"
-export "FLUTTER_BUILD_NUMBER=2.0.2"
+export "FLUTTER_BUILD_NAME=5.1.2"
+export "FLUTTER_BUILD_NUMBER=5.1.2"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
 export "PACKAGE_CONFIG=.dart_tool/package_config.json"
